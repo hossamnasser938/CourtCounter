305b35ab01a0b4325e9c42927e281b99285a5d0a
Hosam <hossmnasser938@gmail.com>
2018-01-27 13:50:22 +0200

    Add methods to display score

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    **This function displays Team A score
+     */
+    void displayA(String score){
+        TextView text = (TextView) findViewById(R.id.teamA_score_text);
+        text.setText(score);
+    }
+
+    /*
+    **This function displays Team A score
+     */
+    void displayB(String score){
+        TextView text = (TextView) findViewById(R.id.teamB_score_text);
+        text.setText(score);
+    }
+
+
+
 }
